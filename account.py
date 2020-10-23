1e879aae762c22521ab8f2768330b5a8befa5721
Vlad Khazin <vladimir.khazin@icssolutions.ca>
2020-10-22 21:05:08 -0400

    adding pseudal code

--- account.py
+++ account.py
@@ -2,7 +2,14 @@
 def open(admin_user_id: str, account: dict) -> dict:
     """
     * User id will be added to account user as admin
+
+    # Validate the account dict contains required keys and values
+    # Convert dict to an insert sql statements: insert account and insert account_user
+    # Leverage internal use class `Repository` to open a connection and execute sql statements within transation
+
+    # Leverage try-except block to log errors before returning an error to the caller
+
+    # Return account dict
     """
-    raise Exception("Not implemented")
 
 # ...more methods...
